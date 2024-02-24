65fdfa5ce682105ee3cd78b71c3b78585a2d310e
pass0210 <pass0210@gmail.com>
2024-02-25 00:52:03 +0900

    Feat: 프로젝트 생성 시 생성자를 멤버에 바로 추가하게 구현 완료

--- src/main/java/com/nhnacademy/springboottask/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/nhnacademy/springboottask/service/impl/ProjectServiceImpl.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.springboottask.service.impl;
 
+import com.nhnacademy.springboottask.domain.Member;
 import com.nhnacademy.springboottask.domain.Project;
 import com.nhnacademy.springboottask.domain.ProjectStatus;
 import com.nhnacademy.springboottask.dto.request.CreateProjectRequest;
+import com.nhnacademy.springboottask.repository.MemberRepository;
 import com.nhnacademy.springboottask.repository.ProjectRepository;
 import com.nhnacademy.springboottask.repository.ProjectStatusRepository;
 import com.nhnacademy.springboottask.service.ProjectService;
@@ -15,10 +17,12 @@ import java.util.List;
 public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
     private final ProjectStatusRepository projectStatusRepository;
+    private final MemberRepository memberRepository;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectStatusRepository projectStatusRepository) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectStatusRepository projectStatusRepository, MemberRepository memberRepository) {
         this.projectRepository = projectRepository;
         this.projectStatusRepository = projectStatusRepository;
+        this.memberRepository = memberRepository;
     }
 
     @Transactional
@@ -28,12 +32,21 @@ public class ProjectServiceImpl implements ProjectService {
         ProjectStatus projectStatus = projectStatusRepository
                 .findById(request.getProjectState())
                 .orElse(null);
+        Member member = new Member();
+        Member.Pk pk = new Member.Pk();
 
         project.setProjectStatus(projectStatus);
         project.setProjectName(request.getProjectName());
         project.setUserId(request.getMemberId());
 
         projectRepository.save(project);
+
+        pk.setProjectId(project.getProjectId());
+        pk.setMemberId(request.getMemberId());
+        member.setPk(pk);
+        member.setProject(project);
+
+        memberRepository.save(member);
     }
 
     @Transactional
